af3d54922d88edaaa321ba6e66af6dcf8daeaeb9
JooYeon Kim <57422057+jysaa5@users.noreply.github.com>
2020-06-22 22:40:16 +0900

    2020.06.22_Add Operation and button action

---
+++ KJY_20200622/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.scientificcalculator;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Operation operator = new Operation();
+
+    Button btn_plus;
+    Button btn_minus;
+    Button btn_multi;
+    Button btn_divide;
+    Button btn_mod;
+
+    Button btn_exp;
+    Button btn_log;
+
+    Button btn_pow;
+    Button btn_factorial;
+
+    TextView tv;
+
+    double result = 0;
+    double x = 5;
+    double y = 3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_plus=(Button) findViewById(R.id.btn_plus);
+        btn_minus=(Button) findViewById(R.id.btn_minus);
+        btn_multi=(Button) findViewById(R.id.btn_multi);
+        btn_divide=(Button) findViewById(R.id.btn_divide);
+        btn_mod=(Button) findViewById(R.id.btn_mod);
+        btn_exp=(Button) findViewById(R.id.btn_exp);
+        btn_log=(Button) findViewById(R.id.btn_log);
+        btn_pow=(Button) findViewById(R.id.btn_pow);
+        btn_factorial=(Button) findViewById(R.id.btn_factorial);
+
+        tv=(TextView) findViewById(R.id.operExpression);
+
+        btn_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText(String.valueOf(operator.plus(x,y)));
+            }
+        });
+
+
+        btn_multi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText(String.valueOf(operator.multiplication(x,y)));
+            }
+        });
+
+        btn_divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText(String.valueOf(operator.division(x,y)));
+            }
+        });
+
+        btn_mod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText(String.valueOf(operator.mod(x,y)));
+            }
+        });
+
+        btn_exp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText(String.valueOf(operator.expFunction(x)));
+            }
+        });
+
+        btn_log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText(String.valueOf(operator.commonLogFunction(x)));
+            }
+        });
+
+
+        btn_minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText(String.valueOf(operator.minus(x,y)));
+            }
+        });
+
+
+        btn_pow.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                tv.setText(String.valueOf(operator.involutionFunction(x, y)));
+            }
+
+        });
+
+        btn_factorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText(String.valueOf(operator.factorialFunction(x)));
+            }
+        });
+
+
+    }
+
+
+}
