cd6a77d04f1096bc04d54c768ba4e372a44a61b7
SeongHwan-Lim <65388334+SeongHwan-Lim@users.noreply.github.com>
2020-06-30 16:25:52 +0900

    팩토리얼 0 입력시 1출력 추가

--- version1.0/Operation.java
+++ version1.0/Operation.java
@@ -48,8 +48,10 @@ public class Operation {
 
     // 팩토리얼 연산
     public double factorialFunction(double x) {
-
-        if (x <= 1) {
+		if (x == 0) {
+			return 1;
+		}
+        elif (x <= 1) {
             return 1;
         } else {
             return factorialFunction(x - 1)*x;
